c6bf469709864d9155a1edcd0f1bdc31fbd32832
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-17 14:33:31 +0200

    Lagd komponenter 💯

---
+++ app/components/SurveyComponent.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { SurveyParticipant, SurveyResponse, SurveyResponses } from './types'; // Importer de tidligere definerte typene
+
+const SurveyComponent: React.FC = () => {
+  const participant: SurveyParticipant = {
+    id: "123",
+    name: "Henriette Navnesen",
+    email: "Henriette@example.com",
+  };
+
+  const response: SurveyResponse = {
+    participant: participant,
+    questionId: "question1",
+    answer: "Ja",
+  };
+
+  const responses: SurveyResponses = {
+    "question1": "Ja",
+    "question2": ["Alternativ 1", "Alternativ 2"],
+    "question3": 42,
+  };
+
+  return (
+    <div>
+      <h2>Deltakerinformasjon</h2>
+      <p>ID: {participant.id}</p>
+      <p>Navn: {participant.name}</p>
+      {participant.email && <p>E-post: {participant.email}</p>}
+
+      <h2>Respons på spørsmål</h2>
+      <p>Spørsmål 1: {response.answer}</p>
+      <p>Spørsmål 2: {responses["question2"].join(", ")}</p>
+      <p>Spørsmål 3: {responses["question3"]}</p>
+    </div>
+  );
+};
+
+export default SurveyComponent;
---
+++ app/components/SurveyPage.tsx
@@ -0,0 +1,14 @@
+// pages/survey.tsx
+import React from "react";
+import SurveyComponent from "../components/SurveyComponent";
+
+const SurveyPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Spørreundersøkelse</h1>
+      <SurveyComponent />
+    </div>
+  );
+};
+
+export default SurveyPage;
