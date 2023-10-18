5bd37f61c9afb432af723c78223603e9ba367e3e
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-18 16:17:08 +0200

    Omrokkert på next-prosjektet 👍

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SurveyComponent from "./components/SurveyComponent";
+import SurveyComponent from "../components/SurveyComponent";
 
 const SurveyPage: React.FC = () => {
   return (
