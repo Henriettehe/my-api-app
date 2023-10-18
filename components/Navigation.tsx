49bb81d5453cd8423bd696a40bc4c2e3b35839d8
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-18 19:41:19 +0200

    😕

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -10,7 +10,7 @@ const Navigation: React.FC = () => {
         <button className="bg-blue-500 text-white px-4 py-2 rounded">Hjem</button>
       </Link>
       {" | "}
-      <Link href="/questions">
+      <Link href="/questions"> {/* Linke til spørsmålene, but how */}
       <button className="bg-blue-500 text-white px-4 py-2 rounded">Spørsmål</button>
       </Link>
     </nav>
