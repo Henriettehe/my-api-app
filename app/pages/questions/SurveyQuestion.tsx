c9ed4d379984dd2a13e92734aacea8a7b9464a08
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-18 19:15:34 +0200

    :keyboard:

--- app/pages/questions/SurveyQuestion.tsx
+++ app/pages/questions/SurveyQuestion.tsx
@@ -2,6 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { SurveyQuestion } from "./questions/SurveyQuestion";
 // Kilde: Kodeeksempel fra Fullstækk - videoforlesninger og forklaring. ++ Google og ChatGpt, og dokumentasjon. Dette gjelder alt av kode! 
 
+// Kommentar: utrolig usikker på dette kompontetet, hadde ønsket å kunne linke det til ./ pathen "questions". 
+// -- for å komme seg på ny side, og besvare spørsmålene under her:  
+
 const questions: SurveyQuestion[] = [
   {
     id: "1",
