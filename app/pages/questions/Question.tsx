375fb57632f3b8742e73322c6ce1a87cc26c700b
HenrietteH <Henriettehestsveen@gmail.com>
2023-10-19 14:06:43 +0200

    Prøvd noe 👍

---
+++ app/pages/questions/Question.tsx
@@ -0,0 +1,12 @@
+import { SurveyQuestion } from 'SurveyQuestion';
+
+const QuestionsPage = () => {
+  return (
+    <div>
+      <h1>Spørsmålssiden</h1>
+      <SurveyQuestion />
+    </div>
+  );
+};
+
+export default QuestionsPage;
